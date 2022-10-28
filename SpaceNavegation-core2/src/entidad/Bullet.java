d526a2992e5b877704d77b9bee5f11f6bc4db58f
benjadiazu <87490211+benjadiazu@users.noreply.github.com>
2022-10-28 11:26:11 -0300

    clases organizadas en paquetes

---
+++ SpaceNavegation-core2/src/entidad/Bullet.java
@@ -0,0 +1,70 @@
+package entidad;
+
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.Sprite;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+
+import interfaces_abs.AbstractJunkObject;
+import interfaces_abs.Movible;
+
+
+public class Bullet implements Movible {
+	private int movementModifier = 5;
+	private boolean destroyed = false;
+	private Sprite spr;
+	    
+	    public Bullet(float x, float y, int xSpeed, int ySpeed, Texture tx) {
+	    	spr = new Sprite(tx);
+	    	spr.setPosition(x, y);
+	    }
+	    
+		public float mover(float x, float y) {
+			// Mover constantemente.
+			y = y + movementModifier;
+			return y;
+			
+		}
+
+		public float checkBorders(float x, float y) {
+			// Verificar si pasó el borde.
+			if (y > bordeSup) {
+				destroyed = true;
+			}
+			return y;
+		}
+		
+		public void setPos (Sprite spr, float x, float y) {
+			y = mover(x, y);
+			y = checkBorders(x, y);
+			spr.setPosition(x, y);
+		}
+		
+	    public void update() {
+	    	//Actualizar el estado de la bala.
+	    	float x = spr.getX();
+			float y = spr.getY();
+	        setPos(spr, x, y);
+	    }
+	    
+	    public void draw(SpriteBatch batch) {
+	    	spr.draw(batch);
+	    }
+	    
+	    public boolean checkCollision(AbstractJunkObject obj) {
+	        if(spr.getBoundingRectangle().overlaps(obj.getArea())){
+	        	System.out.println("olas");
+	            return true;
+	        }
+	        return false;
+	    }
+	    
+		public int getX() {
+			return (int) spr.getX();
+		}
+
+		public int getY() {
+			return (int) spr.getY();
+		}
+
+	    public boolean isDestroyed() {return destroyed;}
+}
