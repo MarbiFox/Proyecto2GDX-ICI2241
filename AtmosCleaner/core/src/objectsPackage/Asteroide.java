e5cc5dd62c5715f07f7e587d9735de42889c897f
MarbiFox <89942465+MarbiFox@users.noreply.github.com>
2022-10-29 12:04:11 -0300

    Avance de Juego Terminado
    - Cambios en Pantallas
    - Detalles arreglados
    - Tutorial agregado

---
+++ AtmosCleaner/core/src/objectsPackage/Asteroide.java
@@ -0,0 +1,69 @@
+package objectsPackage;
+
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.Sprite;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.math.Rectangle;
+
+import abstractInterfacePackage.AbstractJunkObject;
+
+public class Asteroide extends AbstractJunkObject {
+	
+	// Atributo específico
+	private Sprite spr;
+	
+	// Constructor
+	public Asteroide(int x, int y, int xSpeed, Texture tx) {
+		super(x, y, xSpeed);
+		spr = new Sprite(tx);
+		spr.setX(x);
+		spr.setY(y);
+	}
+	
+	// Obtener el área/superficie en el que está el sprite de un objeto.
+	public Rectangle getArea() {
+		return spr.getBoundingRectangle();
+	}
+	
+	// Dibujar el objeto en el batch.
+	public void draw (SpriteBatch batch) {
+		spr.draw(batch);
+	}
+	
+	// Actualizar el movimiento del objeto.
+	public void update() {
+		float x = spr.getX();
+		float y = spr.getY();
+		this.setPos(spr, x, y);
+	}
+
+	@Override
+	// Verificar si la entidad sobrepasa los límites de la pantalla.
+	public float checkBorders (float x, float y) {
+		// TODO Auto-generated method stub
+		if (x - spr.getWidth()/2 > bordeDer) {
+			this.setDestroyed();
+			this.setOut();
+		}
+		return x;
+		
+	}
+
+	@Override
+	// Cambiar los valores de posición de la entidad.
+	public float mover(float x, float y) {
+		// TODO Auto-generated method stub
+		// Actualizar posición
+		x = x + getXSpeed();
+		return x;
+	}
+
+	@Override
+	// Colocar en los nuevos valores de posición a la entidad.
+	public void setPos(Sprite spr, float x, float y) {
+		// TODO Auto-generated method stub
+		x = mover(x, y);
+		x = checkBorders(x, y);
+		spr.setPosition(x, y);
+	}
+}
---
+++ AtmosCleaner/core/src/screensPackage/PantallaJuego.java
@@ -0,0 +1,144 @@
+package screensPackage;
+
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Screen;
+import com.badlogic.gdx.audio.Music;
+import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.OrthographicCamera;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.mygdx.game.AtmosCleaner;
+
+import objectsPackage.GrupoBalas;
+import objectsPackage.Nave;
+import objectsPackage.ObjetosBasura;
+
+public class PantallaJuego implements Screen {
+	
+	// Atributos.
+	private AtmosCleaner game;
+	private OrthographicCamera camera;
+	private SpriteBatch batch;
+	private Music gameMusic;
+	private int score;
+	private float time = 0;
+	private float spawnTime = 1.25f;
+	private Texture tx;
+	private Nave nave;
+	private ObjetosBasura trash = new ObjetosBasura();
+	private GrupoBalas balas = new GrupoBalas();
+	
+	// Constructor.
+	public PantallaJuego(AtmosCleaner game, int score) {
+		// Obtener valores.
+		this.game = game;
+		this.score = score;
+		batch = game.getBatch();
+		
+		// Crear Cámara.
+		camera = new OrthographicCamera();
+		camera.setToOrtho(false, 800, 640);
+		
+		// Inicializar música de fondo.
+		gameMusic = Gdx.audio.newMusic(Gdx.files.internal("piano-loops.wav")); //
+		gameMusic.setLooping(true);
+		gameMusic.setVolume(0.2f);
+		gameMusic.play();
+
+		// Inicializar nave.
+		nave = new Nave(Gdx.graphics.getWidth() / 2 - 50, 30, new Texture(Gdx.files.internal("MainShip3.png")), balas);
+	}
+
+	public void crearFondo() {
+		// Dibujar fondo.
+		batch.draw(tx,0,0,Gdx.graphics.getWidth(),Gdx.graphics.getHeight());
+		// Ajustar Letra.
+		game.getFont().getData().setScale(2f);
+		// Dibujar información.
+		game.getFont().draw(batch, "Score:" + this.score, Gdx.graphics.getWidth() - 180, 790);
+		game.getFont().draw(batch, "HighScore:" + game.getHighScore(), Gdx.graphics.getWidth() / 2 - 80, 790);
+		game.getFont().draw(batch, "Basura Acumulada:" + trash.getCantBasura() + "/100", 10, 790);
+	}
+	
+	@Override
+	public void render(float delta) {
+		// Preparación y Limpieza de Batch.
+		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+		batch.begin();
+		
+		// Dibujar Fondo.
+		crearFondo();
+
+		// Crear Asteroides
+		time += delta;
+		if (time >= spawnTime) {
+			// Crear Basura.
+			trash.generarBasura();
+			time = 0;
+		}
+		
+		// Actualizar Movimientos.
+		balas.updateBullets(batch);
+		trash.updateMovement(batch);
+		nave.draw(batch, this);
+		
+		// Revisar las colisiones entre balas y objetos.
+		score += trash.checkColisionBala(batch, balas);
+		
+		// Revisar si el jugador perdió.
+		if (trash.getCantBasura() >= 100) {
+			if (score > game.getHighScore())
+				game.setHighScore(score);
+			Screen ss = new PantallaGameOver(game);
+			ss.resize(1200, 800);
+			game.setScreen(ss);
+			dispose();
+		}
+		
+		//Terminar Batch.
+		batch.end();
+	}
+
+	@Override
+	public void show() {
+		// TODO Auto-generated method stub
+		gameMusic.play();
+		tx = new Texture(Gdx.files.internal("fondo1.png"));
+	}
+
+	@Override
+	public void resize(int width, int height) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void pause() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void resume() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void hide() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void dispose() {
+		// TODO Auto-generated method stub
+		this.gameMusic.dispose();
+		this.tx.dispose();
+	}
+	
+	
+	//CharSequence str = "Vidas: " + nave.getVidas() + " Ronda: " + ronda;
+	//game.getFont().draw(batch, str, 10, 30);
+
+}
